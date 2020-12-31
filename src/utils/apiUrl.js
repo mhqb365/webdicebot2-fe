f75307048d264564e145f7339a008ffd575ed9ae
mhqb365 <mhqb365@gmail.com>
2021-01-01 00:05:18 +0700

    new-deploy

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,6 +1,6 @@
 const URL = {
-    DEV: 'http://localhost:8002',
-    PROD: 'https://api2.webdicebot.xyz'
+    DEV: 'http://localhost:8001',
+    PROD: 'https://api.webdicebot.xyz'
 }
 
 const API_URL = window.location.hostname == 'localhost' ? URL.DEV : URL.PROD
